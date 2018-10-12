d937d5aeb092de88ef49e67647b913d13d820da7
Tariq Kirsten <tariqkirsten@outlook.com>
2018-10-12 02:14:22 +0200

    Added the functionality to move the borderless window

--- UniversalInventories/MainForm.vb
+++ UniversalInventories/MainForm.vb
@@ -46,4 +46,26 @@ Public Class MainForm
         displayPanel.Controls.Clear()
         displayPanel.Controls.Add(dataPanel)
     End Sub
+
+    Dim draggable As Boolean
+    Dim mouseX As Integer
+    Dim mouseY As Integer
+
+    Private Sub panelTop_MouseDown(sender As Object, e As MouseEventArgs) Handles controlPanel.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub panelTop_MouseMove(sender As Object, e As MouseEventArgs) Handles controlPanel.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub panelTop_MouseUp(sender As Object, e As MouseEventArgs) Handles controlPanel.MouseUp
+        draggable = False
+    End Sub
+
 End Class
